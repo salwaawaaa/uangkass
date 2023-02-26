49bf0dc38720eca3e555e1728e38ac21122a7cf9
zxydeliaputri <zxydliaaptrii@gmail.com>
2023-02-26 15:40:00 +0700

    new layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,79 @@
 <template>
-    <div class="">
-        <div class="container pt-5">
-            <div class="card shadow">
-                <div class="card-header bg-secondary">
-                    <h1 class="text-white fw-bolder">Selamat Datang Bendahara</h1>
-                </div>
-    <div class="card-body">
-      <NuxtLink to="/masuk" class="btn btn-primary my-3 fw-semibold">Masuk</NuxtLink>
-      <table class="table mt-2 table-striped nowrap table-border">
-        <thead>
-          <tr class="text-white bg-secondary">
-            <th>No</th>
-            <th>nama</th>
-            <th>tanggal</th>
-            <th>nominal</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(visitor, i) in datas" :key="visitor.id">
-            <td>{{ i + 1 }}</td>
-            <td>{{ visitor.id_siswa.nama }}</td>
-            <td>{{ visitor.tanggal }}</td>
-            <td>{{ visitor.nominal }}</td>
-          </tr>
-        </tbody>
-      </table>
-      </div>
+  <div class="">
+    <div class="container">
+      <div class="card mt-5">
+        <div class="card-header bg-dark">
+          <h2 class="text-light fw-bolder">Selamat Datang Bendahara</h2>
+        </div>
+        <div class="card-body">
+          <div class="col-md-4 py-2">
+            <NuxtLink to="/masuk" class="btn btn-info text-white mb-2 fw-semibold text-right">Masuk</NuxtLink>
+            <select v-model="minggu" class="form-control">
+              <option v-for="week in weeks" :key="week.id" :value="week.minggu">{{ week.minggu }}</option>
+            </select>
+          </div>
+
+          <div class="text-muted">ada {{ dataFilter.length }} data di {{ minggu }}</div>
+          <table class="table table-striped">
+            <thead>
+              <tr class="text-white bg-dark">
+                <th>No</th>
+                <th>nama</th>
+                <th>minggu</th>
+                <th>nominal</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-if="dataFilter < 1">
+                <td colspan="4">tidak ada data kas</td>
+              </tr>
+
+              <tr v-for="(visitor, i) in dataFilter" :key="visitor.id">
+                <td>{{ i + 1 }}.</td>
+                <td>{{ visitor.id_siswa.nama }}</td>
+                <td>{{ visitor.id_week.minggu }}</td>
+                <td>{{ visitor.nominal }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script setup>
 const supabase = useSupabaseClient();
+const supa = useSupabaseAuthClient();
 const datas = ref([]);
+const weeks = ref([]);
+const minggu = ref("minggu 1");
+const router = useRouter();
+
+console.log(router.currentRoute.value);
 
 async function getData() {
-  const { data, error } = await supabase
-    .from("kas")
-    .select(
-      `
-  id, tanggal, nominal, id_siswa(nama)
-  `
-    )
-    .order("id", { ascending: false });
-
+  const { data, error } = await supabase.from("kas").select(`id, tanggal, nominal, id_siswa(nama) , id_week(id, minggu)`);
   datas.value = data;
 }
+async function getWeek() {
+  const { data, error } = await supabase.from("week").select("id,minggu");
+  weeks.value = data;
+}
+function logout() {
+  const { error } = supa.auth.signOut();
+  navigateTo("/masuk");
+}
+
+const dataFilter = computed(() => {
+  return datas.value.filter((i) => {
+    return i.id_week.minggu.includes(minggu.value);
+  });
+});
 
 onMounted(() => {
   getData();
+  getWeek();
+  //dataFilter()
 });
 </script>
--- pages/masuk.vue
+++ pages/masuk.vue
@@ -2,20 +2,17 @@
   <div>
     <div class="col-md-4 m-auto p-3 mt-5">
       <div class="card">
-        <div class="card-header bg-secondary"><h3 class="text-center fw-bolder text-white">LOGIN</h3></div>
+        <div class="card-header bg-dark"><h3 class="text-center fw-bolder text-white">LOGIN</h3></div>
         <div class="card-body">
           <div v-if="err" class="alert alert-danger">Email/Password Salah</div>
           <form @submit.prevent="masuk()">
-        <br />
-        <label for="email" class="text-dark fw-bold upper">Email</label>
-        <input v-model="email" id="email" type="email" class="form-control" />
-        <br />
-        <br /><label for="pass" class="text-dark fw-bold">Password</label><input v-model="password" id="pass" type="password" class="form-control" />
-        <br />
-        <br /><button class="btn btn-primary me-3 fw-semibold">masuk</button>
-        
-          <NuxtLink to="/daftar" class="btn btn-info text-dark fw-semibold">SignUp</NuxtLink>
-      </form>
+            <label for="email" class="text-dark fw-bold upper mt-3">Email</label>
+            <input v-model="email" id="email" type="email" class="form-control" />
+
+            <label for="pass" class="text-dark fw-bold mt-3">Password</label><input v-model="password" id="pass" type="password" class="form-control" />
+
+            <button class="btn btn-primary me-3 fw-semibold mt-3">masuk</button>
+          </form>
         </div>
       </div>
     </div>
@@ -24,7 +21,7 @@
 
 <script setup>
 const client = useSupabaseAuthClient();
-const email  = ref();
+const email = ref();
 const password = ref();
 
 async function masuk() {
